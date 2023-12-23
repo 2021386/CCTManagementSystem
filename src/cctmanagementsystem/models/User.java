f86857d54fe29480f68ae63ee28d5a9aa13f0a1f
2021386 <2021386@student.cct.ie>
2023-12-23 10:38:11 +0000

    Commit # 12
    Made a user class in .core package and created a function to create a self user account.

---
+++ src/cctmanagementsystem/models/User.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cctmanagementsystem.models;
+
+/**
+ *
+ * @author Asad
+ */
+public class User {
+    // function to create a self user account
+    public void createSelfUserAccount() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("*********** Create User Account ***********");
+        System.out.print("Enter Username: ");
+        String username = scanner.nextLine();
+        username = "ooc2023_" + username;
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+        System.out.print("Enter Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter Surname: ");
+        String surname = scanner.nextLine();
+        System.out.print("Enter Email: ");
+        String email = scanner.nextLine();
+
+        if (checkUsernameExists(connection, username)) {
+            System.out.println("Username already exists. Please choose a different username.");
+            
+            return;
+        }
+        // Insert the newly created user into the database
+        String insertQuery = "INSERT INTO users (username, password, name, surname, email) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, name);
+            preparedStatement.setString(4, surname);
+            preparedStatement.setString(5, email);
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("User account created successfully.");
+            } else {
+                System.out.println("Failed to create user account.");
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("ERROR : " + e.getMessage());
+        }
+
+    }
+
+    
+}
