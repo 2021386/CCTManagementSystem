53df5f4f3e7015b806d9e931b4ed8e52a232e7a8
Muhammad Asad <2021386@student.cct.ie>
2023-12-23 09:47:58 +0000

    Commit # 4
    Made a function in the database class to connect with SQL database.

--- src/cctmanagementsystem/config/Database.java
+++ src/cctmanagementsystem/config/Database.java
@@ -8,6 +8,40 @@ package cctmanagementsystem.config;
  *
  * @author muham
  */
+
+import java.sql.*;
+
 public class Database {
+    // JDBC connection URL for MYSQL database 
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/cctmanagementsystem";
+    //Database user credentials 
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
+    // Singleton instance of the Database class 
+    private static Database instance;
+    // Connection object to manage the database connection 
+    private Connection connection;
+    // Private constructor to restrict instantiation outside the class 
+    private Database(){
+        try {
+            //Establishing connection to the database using JDBC
+            connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            // Handle my SQL exceptions that may occur during connection initialization
+            System.out.println("SQL EXCEPTION DATABASE CONSTRUCTOR : " + e.getMessage());
+        };
+    }
+    // Singleton pattern to ensure only one instance of database exists
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+    // Getter method to provide access to the database connection
+    public Connection getConnection() {
+        return connection;
+    }
     
 }
+
