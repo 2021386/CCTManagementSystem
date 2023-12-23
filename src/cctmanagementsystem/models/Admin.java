376c65ab3d62774fe2993aaafccb539655dcb6ab
Muhammad Asad <2021386@student.cct.ie>
2023-12-23 09:50:28 +0000

    Commit # 5
    Created a function to display the menu for admin.

--- src/cctmanagementsystem/models/Admin.java
+++ src/cctmanagementsystem/models/Admin.java
@@ -30,5 +30,46 @@ public class Admin {
         }
 
     }
+     // function to display the menu for admin
+    public void displayAdminMenu() {
+        // Display the admin menu
+        int adminChoice;
+        do {
+            System.out.println("*********** Welcome, Admin! ***********");
+            System.out.println("1) View User List");
+            System.out.println("2) Modify Admin Profile");
+            System.out.println("3) Remove User");
+            System.out.println("4) Review User Operations");
+            System.out.println("5) Exit");
+            System.out.print("Enter your choice: ");
+            // switch for the choice of admin
+            adminChoice = User.getInstance().getUserChoice();
+            switch (adminChoice) {
+                case 1:
+                    // Display users list
+                    displayUserList();
+                    break;
+                case 2:
+                    // Modify Admin
+                    modifyAdminProfile();
+                    break;
+                case 3:
+                    // Remove user
+                    removeUser();
+                    break;
+                case 4:
+                    viewUserOperations();
+                    break;
+                case 5:
+                    System.out.println("Exiting Admin Menu!");
+                    System.out.println("------------------------------------------------------");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+        } while (adminChoice != 5);
+    }
+
     
 }
