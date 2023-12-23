3b7111a334704cbdf97003b5027092af2d1a1bd6
Muhammad Asad <2021386@student.cct.ie>
2023-12-23 09:39:48 +0000

    Commit # 1
    Made a function for the login of Admin

--- src/cctmanagementsystem/models/Admin.java
+++ src/cctmanagementsystem/models/Admin.java
@@ -9,5 +9,26 @@ package cctmanagementsystem.models;
  * @author muham
  */
 public class Admin {
+    // function for the login of Admin
+    public boolean loginAsAdmin() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Admin Username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter Admin Password: ");
+        String password = scanner.nextLine();
+        
+        // connect to the database
+        String query = "SELECT * FROM admin WHERE username = ? AND password = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            return false;
+        }
+
+    }
     
 }
